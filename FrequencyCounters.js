4f4c69d6ff78fe024407167c666cc092193ed548
marcusp619 <marcusp619@gmail.com>
2019-01-25 09:08:08 -0700

    Add problem

--- FrequencyCounters.js
+++ FrequencyCounters.js
@@ -4,3 +4,10 @@
 + Thic can often avoid the need for nested loops or O(N^2) operations with arrays/strings
 */
 
+/*
+  Write a fn called same, which accepts two arrays.
+  The fn should return true if every value in the array has 
+  its correspond value squared in the 2nd array. 
+  The frequency of values must be the same
+*/
+
