c0a6c670ff22ace67d0aded223636bee08eca3f9
marcusp619 <marcusp619@gmail.com>
2019-01-25 09:06:22 -0700

    first commit

---
+++ FrequencyCounters.js
@@ -0,0 +1,6 @@
+/* 
+* Frequency Counters
+- This pattern uses objects or sets to collect values/frequencies of values
++ Thic can often avoid the need for nested loops or O(N^2) operations with arrays/strings
+*/
+
