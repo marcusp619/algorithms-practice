40aafc3d4f76cb2c087dbafca82739499de2bd8f
marcusp619 <marcusp619@gmail.com>
2019-01-28 10:34:23 -0700

    Add naive solution for multiple pointers

---
+++ MultiplePointers.js
@@ -0,0 +1,22 @@
+/** Multiple Pointers
+ * Creating pointers or values that correspond to an index or
+ * position and move towards the beginning, end or middle
+ * based on a certain condition
+ * Very efficient for solving problems with minimal space complexity as well
+ */
+
+ /**
+  Write a fn called sumZero which accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist
+  **/
+
+//Naive solution
+// time complexity: O(N^2) Space Complexity - O(1)
+function sumZero(arr) {
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = i+1; j < arr.length; j++) {
+      if(arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+}
