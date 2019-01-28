a76d6f6ab49d7f5dce29eef0220a3aef8e04b9cd
marcusp619 <marcusp619@gmail.com>
2019-01-28 10:40:18 -0700

    Add refactor multiple pointer fn

--- MultiplePointers.js
+++ MultiplePointers.js
@@ -20,3 +20,19 @@ function sumZero(arr) {
     }
   }
 }
+
+//Refactored
+function sumZeroR(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while(left < right) {
+    let sum = arr[left] + arr[right];
+    if(sum === 0){
+      return [arr[left], arr[right]];
+    } else if(sum > 0){
+      right --;
+    } else {
+      left++;
+    }
+  }
+}
